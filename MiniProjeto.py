22dcd28a9937a145607fb79bd41099b832b54efa
mauricio047 <bigoli95@gmail.com>
2026-08-12 16:19:34 -0300

    Update MiniProjeto.py

--- MiniProjeto.py
+++ MiniProjeto.py
@@ -20,3 +20,21 @@ dados['DATA'] = pd.to_datetime(dados['DATA'], errors='coerce')
 
 #Remover colunas vazias
 dados = dados.dropna(axis=1, how='all')
+
+#Dados da coluna de Filhos
+contagem = dados['CL_FHL'].count()
+media = dados['CL_FHL'].mean()
+mediana = dados['CL_FHL'].median()
+desvio_padrao = dados['CL_FHL'].std()
+moda = dados['CL_FHL'].mode()[0]
+minimo = dados['CL_FHL'].min()
+maximo = dados['CL_FHL'].max()
+
+print(f"Total de Filhos: {contagem}")
+print(f"Média de Filhos: {media:.2f}")
+print(f"Mediana de Filhos: {mediana}")
+print(f"Desvio Padrão de Filhos: {desvio_padrao:.2f}")
+print(f"Moda de Filhos: {moda}")
+print(f"Mínimo de Filhos: {minimo}")
+print(f"Máximo de Filhos: {maximo}")
+
