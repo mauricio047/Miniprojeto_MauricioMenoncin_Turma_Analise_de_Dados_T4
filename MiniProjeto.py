efc4ecca7c80ef5d519626a679f6c430c64631c6
mauricio047 <bigoli95@gmail.com>
2026-08-17 14:40:59 -0300

    Pronto

--- MiniProjeto.py
+++ MiniProjeto.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import datetime as dt
+import matplotlib.pyplot as plt
 
 #Carregando os dados
 dados = pd.read_csv('Base_Varejo.csv', sep=";")
@@ -38,3 +39,46 @@ print(f"Moda de Filhos: {moda}")
 print(f"Mínimo de Filhos: {minimo}")
 print(f"Máximo de Filhos: {maximo}")
 
+#Quartis
+q25 = dados['CL_FHL'].quantile(0.25)
+q50 = dados['CL_FHL'].quantile(0.50)  # Equivalente à mediana
+q75 = dados['CL_FHL'].quantile(0.75)
+
+print(f"1º Quartil (25%): {q25}")
+print(f"2º Quartil (50% - Mediana): {q50}")
+print(f"3º Quartil (75%): {q75}")
+
+
+#Vendas por gênero
+vendas_por_genero = dados.groupby('CL_GENERO')['CO_ID'].count()
+print("\n--- Compras por Gênero ---")
+print(vendas_por_genero)
+
+#Categorias mais vendidas
+vendas_por_categoria = dados.groupby('PR_CAT')['CO_ID'].count().sort_values(ascending=False)
+print("\n--- Top Categorias Mais Vendidas ---")
+print(vendas_por_categoria.head(5))
+
+#Separando vendas por gênero e top 5 categorias mais vendidas
+
+vendas_por_genero = dados['CL_GENERO'].value_counts()
+top_categorias = dados['PR_CAT'].value_counts().head(5)
+
+#Fazendo a estrutura do gráfico
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+
+#Desenhando o gráfico de barras
+vendas_por_genero.plot(kind='bar', ax=ax1, color='blue', edgecolor='black')
+ax1.set_title('Volume de Compras por Gênero')
+ax1.set_xlabel('Gênero')
+ax1.set_ylabel('Quantidade')
+ax1.tick_params(axis='x', rotation=0)
+
+#Gráfico por top 5 categorias
+top_categorias.plot(kind='barh', ax=ax2, color='green', edgecolor='black')
+ax2.set_title('Top 5 Categorias Mais Vendidas')
+ax2.set_xlabel('Quantidade de Vendas')
+ax2.set_ylabel('Categoria')
+ax2.invert_yaxis()
+
+plt.show()
